fcdec393f80c5864767a72ad12e4c5100f606f96
pk3741 <przemek3741@gmail.com>
2023-01-15 17:05:39 +0100

    Added differential equation function

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hi")
+def differential_equation(x: float, y: float) -> float:
+    """
+    Any differential equation
+    Args:
+        x (int): argument
+        y (int): y argument
+    Returns:
+        float: equation output
+    """
+    return x+x*y+y+1
+
+
+
+
+
+
+
+
+
